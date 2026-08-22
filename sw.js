7b05d67908969fa06f5b1a721ecd103b55b3cfff
ohhaemal-art <ohhaemal@gmail.com>
2026-08-22 21:25:58 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = "sua-spelling-v1";
+const PAGE  = "./index.html";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.add(PAGE)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(hit => {
+      if (hit) return hit;
+      return fetch(e.request).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return resp;
+      }).catch(() => caches.match(PAGE));
+    })
+  );
+});
